ad62b62a9824571013464802c4528d5cd5ae0296
AlisonJardon <alison.jardon@gmail.com>
2019-11-05 22:11:04 +0100

    last commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "eventbrite-v2_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'aj-thp-eventbrite-project.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://aj-thp-eventbrite-project.herokuapp.com/' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
